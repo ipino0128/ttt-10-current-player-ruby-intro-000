9e60b56be47c030d0a1ae6e6c26d3492ff6b6b09
ipino0128@gmail.com <ipino0128@gmail.com>
2018-08-02 16:14:55 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -4,14 +4,9 @@
 def turn_count(board)
   #this counter keeps track of how many occupied spcaes there are
   counter = 0
-  iterations = 0
   board.each do |turn|
-    if board[iterations] == " "
-      counter += 1
-      iterations += 1
-    elsif board[iterations] == "X" || board[counter] == "O"
-      iterations +=1
-  end
+    if board == " "
+      
 end
 end
 
@@ -20,5 +15,3 @@ end
 
 #method that will return, based on that information, an "X" if it is player "X"'s turn and an "O" if it is player "O"'s turn
 #current_player
-
-

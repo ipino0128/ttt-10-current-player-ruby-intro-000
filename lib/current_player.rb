8939d959cd2d6eae53530981fc2bc07edabc1803
ipino0128@gmail.com <ipino0128@gmail.com>
2018-08-02 15:05:15 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,7 @@
-#method that will tell us how many turns have been played 
+#method that will tell us how many turns have been played
 #turn_count
-
+def turn_count(board)
+  
 
 
 #method that will return, based on that information, an "X" if it is player "X"'s turn and an "O" if it is player "O"'s turn

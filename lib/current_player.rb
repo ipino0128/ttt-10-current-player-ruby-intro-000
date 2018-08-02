fb56b07cfbf8e1e50c76cb0f682a9920bafab404
ipino0128@gmail.com <ipino0128@gmail.com>
2018-08-02 15:36:50 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,12 +3,13 @@
 #board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def turn_count(board)
   counter = 0
-  board.each do |turn|
-    if |turn| = "X"
+  board.each do |board|
+    if |board| = "X"
       puts "O"
     end
       counter += 1
     end
+end
 
 
 
